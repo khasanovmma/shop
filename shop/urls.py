30652052a928f227f9df0f0af2190f465f21fb87
khasanovmma <khasanovmma010@gmail.com>
2022-06-25 18:16:57 +0500

    create urls store app

--- shop/urls.py
+++ shop/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', include('store.urls'))
+
 ]
 
 
